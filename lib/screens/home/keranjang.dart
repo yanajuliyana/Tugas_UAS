dbcd5b7523625705eb1fc114d2be4846d8a59a03
yana juliyana <yanajuliyana23@gmail.com>
2021-01-27 11:20:23 +0700

    nweubah warna login,daftar,abaut

--- lib/screens/home/keranjang.dart
+++ lib/screens/home/keranjang.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class Keranjang extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp();
   }
 }
